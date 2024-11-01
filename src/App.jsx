779f6f80884207151cc94be7f6b14a4e83c79255
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-11-01 08:59:48 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import DaisyNav from "./components/DaisyNav/DaisyNav";
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
   return (
     <>
+      {/* <DaisyNav /> */}
+      <Navbar/>
       <h1 className="text-7xl bg-rose-600">Hello World</h1>
     </>
   );
